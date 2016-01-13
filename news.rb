881f3a3a2c8a483cabb3465260982a34418a6bf1
masami.fujimaki <masami.fujimaki@gmail.com>
2016-01-13 16:55:40 +0900

    update

--- news.rb
+++ news.rb
@@ -7,9 +7,9 @@ require 'pp'
 
 def update_dictionary(dictionary, nouns, news)
   nouns.each { |noun|
-    dic = dictionary.find({:noun => noun}).limit(1)
+    dic = dictionary.find({:noun => noun[:noun]}).limit(1)
     if dic.count == 0 
-      dictionary.insert_one({:noun => noun, :count => 1, :news_ids => [news[:_id]]})
+      dictionary.insert_one({:noun => noun[:noun], :feature => noun[:feature], :count => 1, :news_ids => [news[:_id]]})
     else
       dic.each { |e|
         id = e[:_id]
@@ -25,33 +25,40 @@ def update_dictionary(dictionary, nouns, news)
   }
 end
 
+
+def revise_text(text)
+  text.gsub(/【.*?】/, '')
+end
+
 def analysis(db, category, dictionary, natto)
   db[:news].find(:category => category).sort(:date => 1).each_with_index { |news, idx|
     puts "%06d-%s:%s" % [idx, news['category'], news['date']]
     nodes = []
-    natto.parse(news['text']) { |n|
+    natto.parse(revise_text(news['text'])) { |n|
       nodes << { :surface => n.surface, :features => n.feature.split(",") }
     }
 
     nouns = []
     noun = nil
+    feature = nil
     nodes.each { |node|
       surface = node[:surface]
       features = node[:features]
       if features[0] == "名詞"
-        if (features[1] == "接尾" or features[1] == "サ変接続")
+        if features[1] == "接尾" or features[1] == "サ変接続" or features[1] == "数"
           noun = (noun ? noun + surface : surface)
-        elsif features[1] == "数"
-          noun = (noun ? noun + surface : surface)
-        elsif features[1] == "非自立" or features[1] == "代名詞" or features[1] == "副詞可能"
+          feature = (feature ? feature + features[1] : features[1])
+        elsif features[1] == "非自立" or features[1] == "代名詞"
           next
         else 
-          nouns << noun if noun
+          nouns << {:noun=>noun, :feature=>feature} if noun
           noun = surface
+          feature = features[1]
         end 
       else
-        nouns << noun if noun
+        nouns << {:noun=>noun, :feature=>feature} if noun
         noun = nil
+        feature = nil
       end
     } 
 
@@ -66,9 +73,10 @@ db = Mongo::Client.new(['127.0.0.1:27017'], :database=>'sankei')
 natto = Natto::MeCab.new
 
 #['affairs','economy','entertainments','life','politics','sports','world'].each{ |category|
-['entertainments','life','politics','sports','world'].each{ |category|
+['world'].each{ |category|
   dictionary = db[category]
   dictionary.drop()
   dictionary.indexes.create_one({:noun=>1}, :unique=>true)
+  dictionary.indexes.create_one({:count=>1})
   analysis(db, category, dictionary, natto)
 }
